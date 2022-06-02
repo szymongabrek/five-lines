8bdc5afe86f7bf0664500bf5fdde2c20406204fd
Szymon Gabrek <szymon.gabrek@gmail.com>
2022-06-02 19:51:47 +0200

    get rid of if else from handleInput

--- index.ts
+++ index.ts
@@ -19,49 +19,31 @@ enum RawInput {
 }
 
 interface Input {
-  isRight(): boolean;
-  isLeft(): boolean;
-  isUp(): boolean;
-  isDown(): boolean;
   handle(): void;
 
 }
 
 class Right implements Input {
-  isRight() { return true; }     
-  isLeft() { return false; }     
-  isUp() { return false; }       
-  isDown() { return false; }   
+
   handle() {
     moveHorizontal(1);
   }
 }
 
 class Left implements Input {
-  isRight() { return false; }     
-  isLeft() { return true; }     
-  isUp() { return false; }       
-  isDown() { return false; }
+
   handle() {
     moveHorizontal(-1);
   }   
 }
 
 class Down implements Input {
-  isRight() { return false; }     
-  isLeft() { return false; }     
-  isUp() { return false; }       
-  isDown() { return true; }
   handle() {
     moveVertical(1);
   }     
 }
 
 class Up implements Input {
-  isRight() { return false; }     
-  isLeft() { return false; }     
-  isUp() { return true; }       
-  isDown() { return false; } 
   handle() {
     moveVertical(-1);
   }     
@@ -142,14 +124,7 @@ function handleInputs() {
 }
 
 function handleInput(input: Input) {
-  if (input.isLeft())
-    moveHorizontal(-1);
-  else if (input.isRight())
-    moveHorizontal(1);
-  else if (input.isUp())
-    moveVertical(-1);
-  else if (input.isDown())
-    moveVertical(1);
+  input.handle()
 }
 
 function updateMap() {
